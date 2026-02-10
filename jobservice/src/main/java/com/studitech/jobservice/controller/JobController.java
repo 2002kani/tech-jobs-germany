f2911f5352d7660f9f3bffa0422cb84f4a67cced
02Kani <kanikaradag02@gmail.com>
2026-02-11 00:08:22 +0100

    #

--- jobservice/src/main/java/com/studitech/jobservice/controller/JobController.java
+++ jobservice/src/main/java/com/studitech/jobservice/controller/JobController.java
@@ -1,4 +1,19 @@
 package com.studitech.jobservice.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/jobs")
 public class JobController {
+
+    @PostMapping("/bulk")
+    public ResponseEntity<Void> ingestAggregatedJobs(){
+
+    }
 }
