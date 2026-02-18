88b1645b441e6d430f87c549a8dd7670d8bea49e
Kani <kanikaradag02@gmail.com>
2026-02-18 21:42:48 +0100

    Update JobserviceApplication.java

--- jobservice/src/main/java/com/studitech/jobservice/JobserviceApplication.java
+++ jobservice/src/main/java/com/studitech/jobservice/JobserviceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JobserviceApplication {
+public class JobserviceApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobserviceApplication.class, args);
